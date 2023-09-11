ca421be44371b878b5c270bc960ad7ecd91acbe6
ZhangXubo <zhangxubo-cn@qq.com>
2023-09-11 15:25:51 +0800

    添加测试spring回滚

--- src/main/java/com/example/demo/test/controller/TestController.java
+++ src/main/java/com/example/demo/test/controller/TestController.java
@@ -5,9 +5,11 @@ import com.example.demo.common.mapper.UserMapper;
 import com.example.demo.test.sqlsession.datasource1.DataSource1SqlSession;
 import com.example.demo.test.sqlsession.datasource2.DataSource2SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 
 /**
@@ -46,10 +48,6 @@ public class TestController {
             }
             dataSource1SqlSession.insertUserList(users);
             dataSource2SqlSession.insertUserList(users);
-            //预处理 sql,如果有一个数据源数据写入失败,报错,则进入catch全部回滚
-            //简单处理分布式事务
-            dataSource1SqlSession.flushStatements();
-            dataSource2SqlSession.flushStatements();
             //提交事务
             dataSource1SqlSession.commit();
             dataSource2SqlSession.commit();
@@ -65,6 +63,40 @@ public class TestController {
         }
 
     }
+
+    /**
+     * 两个数据源,批量新增提交
+     *
+     * @return
+     */
+    @GetMapping("/testTransactional")
+    @Transactional
+    public String testTransactionalBatchInsert() {
+
+        try {
+            //初始化数据源
+            dataSource1SqlSession.initSqlSession();
+            dataSource2SqlSession.initSqlSession();
+
+            ArrayList<User> users1 = new ArrayList<>();
+            ArrayList<User> users2 = new ArrayList<>();
+            users1.add(new User().setId(BigDecimal.valueOf(1)).setName("张三"));
+            users1.add(new User().setId(BigDecimal.valueOf(2)).setName("李四"));
+            users2.add(new User().setId(BigDecimal.valueOf(1)).setName("张三"));
+            users2.add(new User().setId(BigDecimal.valueOf(1)).setName("李四"));
+            dataSource1SqlSession.insertUserList(users1);
+            dataSource2SqlSession.insertUserList(users2);
+            //提交事务
+            dataSource1SqlSession.commit();
+            dataSource2SqlSession.commit();
+            return "true";
+        } finally {
+            dataSource1SqlSession.closeSqlSession();
+            dataSource2SqlSession.closeSqlSession();
+        }
+
+    }
+
 
     /**
      * 两个数据源,批量新增提交
